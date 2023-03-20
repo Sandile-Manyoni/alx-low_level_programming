03d712e058bb95a6955ed0ca23666bfb1c96d977
SANDILE <SANDIL.ALWANDE@gmail.com>
2023-03-20 14:25:03 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing a line with puts function
+ * Return: 0
+ */
+int main(void)
+{
+putss("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
